9b5f6c3d84a81073adc898cd3658b7c7faf5ba67
Prasanna <prasannapbhurke@gmail.com>
2026-04-18 09:53:27 +0530

    ⚡ Fix: Remove invalid SQLAlchemy import

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_url, create_engine, Column, Integer, String, Boolean, DateTime, Text
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
